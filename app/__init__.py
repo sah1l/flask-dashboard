388e8e73d8c2f607b8c7ec202a8fef6abcaf30bc
Yelena <sigizmoon@list.ru>
2018-02-15 05:36:51 +0500

    Added basic admin panel

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,7 @@
 from flask import Flask
 from flask_login import LoginManager
+from flask_admin import Admin
+from flask_admin.contrib.sqla import ModelView
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
@@ -18,6 +20,13 @@ base = declarative_base()
 # login init
 login = LoginManager(app)
 login.login_view = 'auth.login'
+
+# admin init
+from app.mod_auth.models import User
+
+admin = Admin(app, template_mode='bootstrap3')
+session = session_maker()
+admin.add_view(ModelView(User, session))
 
 # Import module using its blueprint
 from app.mod_stats.controllers import mod_stats as statistics_module
