e66e10cbe21c622266f7b1159e8bbc8ce77ffc17
Yelena <sigizmoon@list.ru>
2018-02-25 20:47:17 +0500

    Fixed login bug when unknown email is used

--- app/mod_auth/forms.py
+++ app/mod_auth/forms.py
@@ -1,7 +1,9 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField, BooleanField, SelectField
-from wtforms.validators import DataRequired, Email, EqualTo, Length
-from wtforms.ext.sqlalchemy.fields import QuerySelectField
+from wtforms import StringField, PasswordField, SubmitField, BooleanField
+from wtforms.validators import DataRequired, Email
+
+from app import session_maker
+from app.mod_auth.models import User
 
 
 class LoginForm(FlaskForm):
@@ -12,3 +14,21 @@ class LoginForm(FlaskForm):
     password = PasswordField('Password', validators=[DataRequired()])
     remember_me = BooleanField('Remember Me')
     submit = SubmitField('Log In')
+
+    def __init__(self, *args, **kwargs):
+        FlaskForm.__init__(self, *args, **kwargs)
+        self.user = None
+
+    def validate(self):
+        rv = FlaskForm.validate(self)
+        if not rv:
+            return False
+
+        session = session_maker()
+        user = session.query(User).filter_by(email=self.email.data).first()
+        if not user:
+            self.email.errors.append("This email is not registered.")
+            session.close()
+            return False
+
+        return True
